2d8c69ddf94953e44290f2a5a3cd581557383c2e
HuGo Portal <45176167+hugoportal@users.noreply.github.com>
2023-08-16 15:17:37 -0300

    Eventos no HTML

---
+++ 02-index.js
@@ -0,0 +1,20 @@
+
+function mouseEnterEvent() {
+    console.log('mouse entered')
+  }
+  
+  function changeEvent() {
+    console.log('value changed')
+  }
+  
+  function focusEvent() {
+    console.log('gained focus')
+  }
+  
+  function blurEvent() {
+    console.log('lost focus')
+  }
+  
+  function clickEvent() {
+    alert('clicked')
+  }
